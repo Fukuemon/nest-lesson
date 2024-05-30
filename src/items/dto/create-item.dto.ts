8cae625c01987352357981c36740d2402a033383
Fukuemon <Fukuemony029@gmail.com>
2024-05-30 11:42:24 +0900

    Class Validatorを追加

--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,5 +1,18 @@
+import { Type } from 'class-transformer';
+import { IsInt, IsNotEmpty, IsString, MaxLength, Min } from 'class-validator';
+
 export class CreateItemDto {
+  @IsString() // 文字列
+  @IsNotEmpty() // 空でない
+  @MaxLength(40) // 40文字以内
   name: string;
+
+  @IsInt() // 整数
+  @Min(1) // 1以上
+  @Type(() => Number) // number型に変換
   price: number;
+
+  @IsString() // 文字列
+  @IsNotEmpty() // 空でない
   description: string;
 }
